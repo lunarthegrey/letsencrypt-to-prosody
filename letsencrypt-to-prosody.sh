d45bf6cbdceb15b5c426d35ec285ec44e6c94544
lunarthegrey <lunar@cypherpunk.is>
2016-07-09 03:26:27 -0400

    Create letsencrypt-to-prosody.sh

---
+++ letsencrypt-to-prosody.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+letsencrypt="/etc/letsencrypt/live/something.com/"
+certs="/etc/prosody/certs/"
+prosody="/etc/prosody/"
+
+   find "${letsencrypt}" -name "*.pem" | while read -r file
+
+do
+
+   cp "${file}" "${certs}"
+   chown -R prosody:prosody "${prosody}"
+   chmod -R 700 "${certs}"
+
+done
