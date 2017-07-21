a3ea17f86bb8a1f81d93ea58e2bfed83c2709e69
Lunar <lunar@cypherpunk.is>
2017-07-21 15:00:11 -0500

    Other method didn't work

--- letsencrypt-to-prosody.sh
+++ letsencrypt-to-prosody.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-DIR=( "domain1.com" "domain2.com" "domain3.com" )
+DIR1="domain1.com"
+DIR2="domain2.com"
+DIR3="domain3.com"
+LE_DIR="/etc/letsencrypt/live/"
 CERTS="/etc/prosody/certs/"
 PROSODY="/etc/prosody/"
 
-   find "/etc/letsencrypt/live/${DIR}" -name "*.pem" | while read -r file
-
-do
-
-   cp "${file}" "/etc/prosody/certs/${DIR}/"
-
-done
-
+   cp -rfL "${LE_DIR}${DIR1}/" "${CERTS}"
+   cp -rfL "${LE_DIR}${DIR2}/" "${CERTS}"
+   cp -rfL "${LE_DIR}${DIR3}/" "${CERTS}"
+   
    chown -R prosody:prosody "${PROSODY}"
    chmod -R 700 "${CERTS}"
    prosodyctl reload
